bf605d7dd479fe3f27e22e36189891a695c6ddab
CBox <support@cbox.co.il>
2021-06-10 10:27:04 +0300

    try or log

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -2,17 +2,48 @@
 
 namespace Flashy\Tests;
 
+use Flashy\Exceptions\FlashyException;
+use Flashy\Flashy;
 use Flashy\Helper;
 
 class HelperTest extends BaseTest
 {
 
     /**
+     * @var Flashy
+     */
+    private $flashy;
+
+    /**
      * @test
+     * @throws FlashyException
      */
     public function get_helper()
     {
+        $this->init();
+
         $this->assertEquals(null, Helper::get("empty", []));
+    }
+
+    /**
+     * @test
+     * @throws FlashyException
+     */
+    public function try_something_or_log()
+    {
+        $this->init();
+
+        $list = 4;
+
+        Helper::tryOrLog(function() use ($list) {
+
+            Helper::dd($list);
+
+            badfunction($as);
+
+        });
+
+        $this->assertTrue(true);
     }
 
 }
